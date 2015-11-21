2006eda6bb68f66de975f88d88f1505c703c0e8a
Prateek Chandan <prateekchandan5545@gmail.com>
2015-11-22 00:38:12 +0530

    added helper

---
+++ project/helper.m
@@ -0,0 +1,17 @@
+pref = 'arborgreens';
+disp(pref);
+
+folder = './data/imagedatabase.cs.washington.edu/groundtruth/australia/';
+images = dir(strcat(folder,'*.jpg'));
+nfiles = length(images);    % Number of files found
+disp(nfiles);
+%im = zeros(nfiles, 4096, 2);
+ccvs = [];
+for ii=1:nfiles
+   currentfilename = images(ii).name;
+   disp(currentfilename);
+   im = CCV(strcat(folder,currentfilename), 16);
+   ccvs = cat(3, ccvs, im);
+end
+
+    
