4c71baf14b7345e1a16565ac717c5378db882e49
Prateek Chandan <prateekchandan5545@gmail.com>
2015-11-23 17:47:09 +0530

    Did centering refinement

--- project/CCV.m
+++ project/CCV.m
@@ -23,11 +23,18 @@ function [vector] = CCV(image_name,factor,THRESHOLD)
     avg_filt = fspecial('average');
     image = imfilter(image, avg_filt);
     image = floor(double(image)/factor);
-    hist = zeros(antifactor*antifactor*antifactor,2);
+    hist = zeros(antifactor*antifactor*antifactor,4);
     s = size(image);
     visited = zeros(s(1),s(2));
-
-   
+    xcenter = s(1)/2;
+    ycenter = s(2)/2;
+    radius = min(s(1),s(2)) * 0.4; % 0.4 means 80% of 0.5 which is finding the radius
+    
+    function [res] = iscenter(x,y)
+       dis = sqrt((x-xcenter)*(x-xcenter) + (y-ycenter)*(y-ycenter)) ;
+       res = dis < radius;
+    end
+    
     function  help(x,y,prevbin)
         if x < 1 || y < 1 || x > s(1) || y > s(2) || visited(x,y)==1
             return
@@ -46,6 +53,12 @@ function [vector] = CCV(image_name,factor,THRESHOLD)
         %disp([x,y,count,bin])
         visited(x,y)=1;
         count = count + 1;
+        if iscenter(x,y) == 1
+            incenter = incenter+1;
+        else
+            outcenter = outcenter+1;
+        end;
+        
         help(x+1,y,bin);
         help(x-1,y,bin);
         help(x,y+1,bin);
@@ -63,14 +76,18 @@ function [vector] = CCV(image_name,factor,THRESHOLD)
     for i = 1:s(1)
         for j = 1:s(2)
             count = 0;
+            incenter = 0;
+            outcenter = 0;
             curbin = 0;
             help(i,j,0);
             if count == 0
                  continue;
             elseif count < THRESHOLD
-                hist(curbin,1) = hist(curbin,1)+count;
+                hist(curbin,1) = hist(curbin,1)+incenter;
+                hist(curbin,2) = hist(curbin,2)+outcenter;
             else
-                hist(curbin,2) = hist(curbin,2)+count;
+                hist(curbin,3) = hist(curbin,3)+incenter;
+                hist(curbin,4) = hist(curbin,4)+outcenter;
             end
         end
     end
